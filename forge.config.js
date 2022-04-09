08e67a4f4d047eab5317b63aa4c9728719163709
Johan Bos <johan@coconote.nl>
2022-04-09 08:03:52 +0200

    Add github publisher

---
+++ forge.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "makers": [
+        {
+            "name": "@electron-forge/maker-squirrel",
+            "config": {
+                "name": "naive_photo_dare_co7bu"
+            }
+        },
+        {
+            "name": "@electron-forge/maker-zip",
+            "platforms": [
+                "darwin"
+            ]
+        },
+        {
+            "name": "@electron-forge/maker-deb",
+            "config": {}
+        },
+        {
+            "name": "@electron-forge/maker-rpm",
+            "config": {}
+        }
+    ],
+    publishers: [
+        {
+            name: '@electron-forge/publisher-github',
+            config: {
+                repository: {
+                    owner: 'Johan Bos',
+                    name: 'Home Assistant Import'
+                },
+                prerelease: true
+            }
+        }
+    ]
+}
+
